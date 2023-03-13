8627200fb25ce82404bc3f41d9f42a014ff11572
Your Name <chiarapigoni@mac-mini.home>
2023-03-13 13:36:14 +0000

    First commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section >
+    <div className="title">
+      <h1>HTML COLORS</h1>
+      
+    </div>
+    <div>
+    <p className="par-header">Insert the HTML color name and get the HEX color code!</p>
+    </div>
+    </section>
+  )
+}
+
+export default Header;
